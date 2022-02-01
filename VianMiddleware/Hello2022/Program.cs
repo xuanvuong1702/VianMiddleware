d90e5d19cf5ce4e4b647375d7c3ba8d799745838
wangyiwu <trachanhvn.tk@gmail.com>
2022-02-01 08:55:23 +0700

    @

---
+++ VianMiddleware/Hello2022/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello 2022!");
+        }
+    }
+}
